49fade22bc6a5bca00e5f06925050445af160aac
duncan <duncanndiithi@yahoo.com>
2021-10-11 20:44:10 +0300

    add questions item to checlist

--- app/Http/Controllers/Service/ReadinessController.php
+++ app/Http/Controllers/Service/ReadinessController.php
@@ -8,6 +8,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ReadinessController extends Controller
 {
@@ -47,6 +48,52 @@ class ReadinessController extends Controller
 
     public function getReadinessSurveyById(Request $request)
     {
-        
+        $user = Auth::user();
+        try {
+
+            $readinesses = Readiness::select(
+                "readinesses.id",
+                "readinesses.start_date",
+                "readinesses.end_date",
+                "readinesses.name",
+                "readinesses.admin_id",
+                "readiness_questions.question",
+                "readiness_questions.answer_options",
+                "readiness_questions.answer_type",
+                "readiness_questions.qustion_position",
+                "readiness_questions.qustion_type",
+
+            )->join('laboratory_readiness', 'laboratory_readiness.readiness_id', '=', 'readinesses.id')
+                ->join('readiness_questions', 'readiness_questions.readiness_id', '=', 'readinesses.id')
+                ->join('laboratories', 'laboratory_readiness.laboratory_id', '=', 'laboratories.id')
+                ->join('users', 'users.laboratory_id', '=', 'laboratories.id')
+                ->where('readinesses.id', $request->id)
+                ->where('users.id', $user->id)->toSql();
+
+
+            $readinesses = Readiness::select(
+                "readinesses.id",
+                "readinesses.start_date",
+                "readinesses.end_date",
+                "readinesses.name",
+                "readinesses.admin_id",
+                "readiness_questions.question",
+                "readiness_questions.answer_options",
+                "readiness_questions.answer_type",
+                "readiness_questions.qustion_position",
+                "readiness_questions.qustion_type",
+
+            )->join('laboratory_readiness', 'laboratory_readiness.readiness_id', '=', 'readinesses.id')
+                ->join('readiness_questions', 'readiness_questions.readiness_id', '=', 'readinesses.id')
+                ->join('laboratories', 'laboratory_readiness.laboratory_id', '=', 'laboratories.id')
+                ->join('users', 'users.laboratory_id', '=', 'laboratories.id')
+                ->where('readinesses.id', $request->id)
+                ->where('users.id', $user->id)
+                ->get();
+
+            return $readinesses;
+        } catch (Exception $ex) {
+            return response()->json(['Message' => 'Could fetch readiness list: ' . $ex->getMessage()], 500);
+        }
     }
 }
--- resources/js/user/pt/participant/Readiness.js
+++ resources/js/user/pt/participant/Readiness.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FetchReadnessSurveyById } from '../../../components/utils/Helpers';
 import { matchPath } from "react-router";
+import { v4 as uuidv4 } from 'uuid';
 
 
 class Readiness extends React.Component {
@@ -9,8 +10,77 @@ class Readiness extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            message: '',
+            id: '',
+            name: '',
+            startDate: '',
+            endDate: '',
+            readinessQuestions: [],
+            readinessItems: [],
+        }
 
+        this.addReadinessQuestion = this.addReadinessQuestion.bind(this);
 
+    }
+
+    addReadinessQuestion(readiness) {
+
+        let rdItems = this.state.readinessItems;
+        rdItems.push(readiness);
+        let itemIndex = rdItems.length - 1;
+        readiness['delete_status'] = 0;
+        if (readiness['qustion_type'] == 'question') {
+
+            if (readiness['answer_type'] == 'list') {
+                let id = uuidv4();
+                let qstOptins = readiness['answer_options'].split(',');
+                let qstElement =
+                    <div key={id} className="form-group">
+
+                        <label className="float-left" htmlFor={id + "qst_answer"}>{readiness['question']}</label>
+                        <select
+                            className="custom-select" id={id + "qst_answer"}>
+                            {qstOptins.map((option) => {
+                                return <option key={uuidv4()} value={option}>{option}</option>
+                            })}
+                        </select>
+                    </div>
+                let questions = this.state.readinessQuestions;
+                questions.push(qstElement);
+                this.setState({
+                    readinessQuestions: questions,
+                    readinessItems: rdItems
+                })
+            } else if (readiness['answer_type'] == 'number') {
+                let id = uuidv4();
+                let qstElement =
+                    <div key={id} className="form-group">
+
+                        <label className="float-left" htmlFor={id + "qst_answer"}>{readiness['question']}</label>
+                        <input type="number" className="form-control" id={id + "qst_answer"} aria-describedby="qst_answer" placeholder="Enter your answer" />
+                    </div>
+                let questions = this.state.readinessQuestions;
+                questions.push(qstElement);
+                this.setState({
+                    readinessQuestions: questions,
+                    readinessItems: rdItems
+                })
+            }
+
+        } else if (readiness['qustion_type'] == 'comment') {
+            let id = uuidv4();
+            let qstElement =
+                <div className="form-group">
+
+                    <label className="float-left" htmlFor={id + "qst_answer"}>{readiness['question']}</label>
+                    <textarea className="form-control" id={id + "qst_answer"} aria-describedby="qst_answer" placeholder="Enter your comment" />
+                </div>
+            let questions = this.state.readinessQuestions;
+            questions.push(qstElement);
+            this.setState({
+                readinessQuestions: questions,
+                readinessItems: rdItems
+            })
         }
 
     }
@@ -31,26 +101,123 @@ class Readiness extends React.Component {
                 });
                 $('#readinessFormModal').modal('toggle');
             } else {
+                let readinessId = null;
 
-                // readinessItems.payload.questions.map((questionItem) => {
-                //     this.addReadinessQuestion(questionItem);
-                // });
+                let startDate = null; "2021-09-29"
+                let endDate = null;
+                let name = null;
+
+                readinessItems.map((questionItem) => {
+                    this.addReadinessQuestion(questionItem)
+                    readinessId = questionItem.id;
+                    startDate = questionItem.start_date;
+                    endDate = questionItem.end_date;
+                    name = questionItem.name;
+                    // addReadinessQuestion(questionItem);
+                });
                 this.setState({
+                    id: readinessId,
+                    name: name,
+                    startDate: startDate,
+                    endDate: endDate
 
                 });
             }
 
-
         })();
-
 
     }
 
     render() {
-
         return (
             <React.Fragment>
-                Hi
+
+
+                <div className="card" style={{ "backgroundColor": "#ecf0f1" }}>
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            Readiness Checklist Survey Form
+                        </h5><br />
+                        <hr />
+                        <div style={{ "margin": "0 auto", "width": "80%" }} className="text-center">
+
+                            <div className="form-group row">
+                                <label htmlFor="u_name" className="col-sm-2 col-form-label">Name/Title</label>
+                                <div className="col-sm-10">
+                                    <input
+                                        value={this.state.name}
+                                        readOnly
+                                        type="email" className="form-control" id="u_name" />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label htmlFor="u_start_date" className="col-sm-2 col-form-label">Start date</label>
+                                <div className="col-sm-10">
+                                    <input
+                                        value={this.state.startDate}
+                                        readOnly
+                                        type="date"
+                                        className="form-control" id="u_start_date" />
+                                </div>
+                            </div>
+
+                            <div className="form-group row">
+                                <label htmlFor="u_end_date" className="col-sm-2 col-form-label">End Date</label>
+                                <div className="col-sm-10">
+                                    <input
+                                        value={this.state.endDate}
+                                        readOnly
+                                        type="date"
+                                        className="form-control" id="u_end_date" />
+                                </div>
+                            </div>
+
+                            <div className="form-row">
+                                <div className="col-sm-2 mb-3">
+                                    <label className="float-left">Checklist Question(s) *</label><br />
+                                </div>
+                                <div className="col-sm-10 mb-3">
+
+                                    <div className="card">
+                                        <div className="card-body">
+                                            {
+                                                this.state.readinessQuestions.map((rdnsQuestion) => {
+
+                                                    return <span key={uuidv4()}>{rdnsQuestion}</span>
+                                                })
+                                            }
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div className="form-group row">
+                                <div className="col-sm-10 mt-3">
+                                    <a onClick={() => this.saveReadiness()} type="" className="d-inline m-2 btn btn-info m">
+                                        Save
+                                    </a>
+                                    <a
+                                        onClick={
+                                            () => {
+                                                window.location.assign('/participant-pt-home')
+                                            }
+                                        }
+                                        className="d-inline m-2 btn btn-danger">Exit</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
                 < div className="modal fade" id="readinessFormModal" tabIndex="-1" role="dialog" aria-labelledby="readinessModalTitle" aria-hidden="true" >
                     <div className="modal-dialog modal-dialog-centered" role="document">
                         <div className="modal-content">
