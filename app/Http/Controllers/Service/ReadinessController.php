434b071e4220c4fc7416c496095f46064fb58b04
duncan <duncanndiithi@yahoo.com>
2021-10-12 17:17:08 +0300

    select disticnt readiness

--- app/Http/Controllers/Service/ReadinessController.php
+++ app/Http/Controllers/Service/ReadinessController.php
@@ -19,27 +19,39 @@ class ReadinessController extends Controller
         $user = Auth::user();
         try {
 
-            $readinesses = Readiness::select(
-                "readinesses.id",
-                "readinesses.start_date",
-                "readinesses.end_date",
-                "readinesses.name",
-                "readinesses.admin_id",
-                "readiness_answers.readiness_id as aswered_id"
+            // $readinesses = Readiness::select(
+
+            $readinesses =   DB::table("readinesses")->distinct(
+                // "readinesses.id",
+                // "readinesses.start_date",
+                // "readinesses.end_date",
+                // "readinesses.name",
+                // "readinesses.admin_id",
+                // "readiness_answers.readiness_id as aswered_id"
+            )
+
                 // "readiness_questions.question",
                 // "readiness_questions.answer_options",
                 // "readiness_questions.answer_type",
                 // "readiness_questions.qustion_position",
                 // "readiness_questions.qustion_type",
 
-            )->join('laboratory_readiness', 'laboratory_readiness.readiness_id', '=', 'readinesses.id')
+                ->join('laboratory_readiness', 'laboratory_readiness.readiness_id', '=', 'readinesses.id')
                 ->join('laboratories', 'laboratory_readiness.laboratory_id', '=', 'laboratories.id')
                 ->join('users', 'users.laboratory_id', '=', 'laboratories.id')
                 ->leftJoin('readiness_answers', 'readinesses.id', '=', 'readiness_answers.readiness_id')
                 // ->join('readiness_questions', 'readiness_questions.readiness_id', '=', 'readinesses.id')
                 ->where('users.id', $user->id)
                 ->orderBy('readinesses.created_at', 'Desc')
-                ->get();
+                ->get([
+                    "readinesses.id",
+                    "readinesses.start_date",
+                    "readinesses.end_date",
+                    "readinesses.name",
+                    "readinesses.admin_id",
+                    "readiness_answers.readiness_id as aswered_id",
+                    "readinesses.created_at"
+                ]);
 
             return $readinesses;
         } catch (Exception $ex) {
